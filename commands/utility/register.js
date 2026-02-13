152ba6b467018d3aed06c4dbb934df6b8d7f305e
calebhoang <calebhoangm@gmail.com>
2026-02-13 17:24:42 -0600

    Updated register.js to properly log new UIDs into MongoDB database

--- commands/utility/register.js
+++ commands/utility/register.js
@@ -1,4 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { MongoClient } = require('mongodb');
+const { databaseConnect } = require('../../config.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -8,8 +10,32 @@ module.exports = {
     .addIntegerOption((option) => option.setName('uid').setDescription('Your UID!').setRequired(true).setMaxValue(999999999))
     ,
 	async execute(interaction) {
+        const client = new MongoClient(databaseConnect);
         const region = interaction.options.getString('region');
         const uid = interaction.options.getInteger('uid');
-		await interaction.reply('Registered!');
+        let reply = null;
+        try {
+            const database = client.db('TA-TA');
+            const collection = database.collection("Users")
+            const query = {id: interaction.user.id};
+            let update;
+            if (region === "asia") {
+                update = {$set: {name: interaction.user.username, uid_a:uid}};
+            } else {
+                update = {$set: {name: interaction.user.username, uid_n:uid}};
+            }
+            const options = {upsert: true};
+            await collection.updateOne(query, update, options)
+        } catch (error) {
+            console.log(error);
+            reply = error;
+        } finally {
+            await client.close();
+        }
+        if (region === 'asia') {
+		    await interaction.reply('Registered Asia UID as: ' + uid);
+        } else {
+            await interaction.reply('Registered NA/EU UID as: ' + uid);
+        }
 	},
 };
