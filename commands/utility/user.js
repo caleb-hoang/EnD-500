48274aa5249f14949cc408492d1e2a9483787b6a
calebhoang <calebhoangm@gmail.com>
2026-02-13 18:12:46 -0600

    Updated user.js to pull information from database

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -1,12 +1,28 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { MongoClient } = require('mongodb');
+const { databaseConnect } = require('../../config.json');
 
 module.exports = {
 	data: new SlashCommandBuilder().setName('user').setDescription('Provides information about the user.'),
 	async execute(interaction) {
-		// interaction.user is the object representing the User who ran the command
-		// interaction.member is the GuildMember object, which represents the user in the specific guild
-		await interaction.reply(
-			`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`,
-		);
+		const client = new MongoClient(databaseConnect);
+		const database = client.db('TA-TA');
+        const collection = database.collection("Users")
+        const query = {id: interaction.user.id};
+		const user_data = await collection.findOne(query);
+		if (user_data === null) {
+			await interaction.reply('No user information found for user ' + interaction.user.username + "!");
+		} else {
+			let reply = "Discord Name: " + interaction.user.username;
+			
+			if ('uid_a' in user_data) {
+				reply += "\nUID (Asia): " + user_data.uid_a;
+			}
+
+			if ('uid_n' in user_data) {
+				reply += "\nUID (NA/EU): " + user_data.uid_n;
+			}
+			await interaction.reply(reply);
+		}
 	},
 };
